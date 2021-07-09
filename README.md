a3a83c11b679548e44e170c9fd9086c13f6ca8c0
Micheal Ataklt <matakltm.code@gmail.com>
2021-07-09 13:25:39 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Online-Web-Based-Census-Management-System
+
+## Commands
+pa migrate:fresh --seed
---
+++ database/seeders/RegionSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Region;
+use Illuminate\Database\Seeder;
+
+class RegionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Create 10 regions using RegionFactory.php file
+        Region::factory()->times(10)->create();
+    }
+}
